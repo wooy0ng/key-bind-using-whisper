5a365560e243340c67619ceaef5f95b30daddd91
wooy0ng <yygg9800@naver.com>
2023-03-08 17:25:37 +0900

    feat: change path loaded

--- dataloader.py
+++ dataloader.py
@@ -1,47 +1,21 @@
 import os
 import torch
 import whisper
-import numpy as np
-import pandas as pd
-import torch.nn.functional as F
 import torchaudio
+import numpy as np
+import torch.nn.functional as F
 
-from torch.utils.data import Dataset
-from collections import defaultdict
+from typing import Dict, List, Union, Tuple
 from torchaudio.utils import download_asset
 from audiomentations import AddGaussianNoise
+from torch.utils.data import Dataset
 from pathlib import Path
-from typing import Union, Tuple, List, Dict
+from collections import defaultdict
 
-r""" 
-download from torchaudio 
-1. 방 잔향 증강
-2. 
-
-"""
 SAMPLING_RATE = 16_000
 RIR: str = download_asset('tutorial-assets/Lab41-SRI-VOiCES-rm1-impulse-mc01-stu-clo-8000hz.wav')
 
-def load_sound_data(wav_path: Union[str, List[str]], return_mel=False) -> Dict[str, List]:
-    if type(wav_path) == str:
-        paths = [wav_path]
-    else:
-        paths = [os.path.join(Path(__file__).parent, path) for path in wav_path]
-
-    dataset = defaultdict(list)
-    for path in paths:
-        audio = whisper.load_audio(path)
-        dataset['path'].append(path)
-        dataset['audio'].append(torch.Tensor(whisper.pad_or_trim(audio)))
-
-    if return_mel:
-        for i in range(len(dataset['path'])):
-            mel = whisper.log_mel_spectrogram(dataset['audio'][i])  # (80, 3000)
-            mel = np.expand_dims(mel, 0)                            # (1, 80, 3000)
-            dataset['mel'].append(torch.Tensor(mel))
-    return dataset
-
-def augment_sound_data(wav_path: str) -> Dict:
+def augment_sound_data(folder_path: str) -> Dict:
     def augment_sound_rir(audio: torch.Tensor) -> List[torch.Tensor]:
         rir = torch.Tensor(whisper.load_audio(RIR)).unsqueeze(0)
         rir = rir[:, int(SAMPLING_RATE * 1.01) : int(SAMPLING_RATE * 1.3)]
@@ -71,7 +45,8 @@ def augment_sound_data(wav_path: str) -> Dict:
         aug = aug.squeeze()
         return aug
 
-    dataset = load_sound_data(wav_path)
+    wav_paths = [os.path.join(folder_path, path) for path in os.listdir(folder_path) if path.split('.')[-1] == 'wav' ]
+    dataset = load_sound_data(wav_paths)
 
     aug_dataset = defaultdict(list)
     for path, audio in zip(dataset['path'], dataset['audio']):
@@ -88,6 +63,25 @@ def augment_sound_data(wav_path: str) -> Dict:
     dataset['path'] += aug_dataset['path']
     dataset['audio'] += aug_dataset['audio']
     
+    return dataset
+
+def load_sound_data(wav_path: Union[str, List[str]], return_mel=False) -> Dict[str, List]:
+    if type(wav_path) == str:
+        paths = [wav_path]
+    else:
+        paths = wav_path
+
+    dataset = defaultdict(list)
+    for path in paths:
+        audio = whisper.load_audio(path)
+        dataset['path'].append(path)
+        dataset['audio'].append(torch.Tensor(whisper.pad_or_trim(audio)))
+
+    if return_mel:
+        for i in range(len(dataset['path'])):
+            mel = whisper.log_mel_spectrogram(dataset['audio'][i])  # (80, 3000)
+            mel = np.expand_dims(mel, 0)                            # (1, 80, 3000)
+            dataset['mel'].append(torch.Tensor(mel))
     return dataset
 
 
--- main.py
+++ main.py
@@ -18,8 +18,8 @@ TODO:
             TODO: Mean Pooling 추가 (X) [폐기]
         TODO: 손실함수 정의
             TODO: 적대적 생성 신경망 (O)
-                TODO: key generate 모델 학습을 더 어렵게 만들기
-    TODO : 랜덤성 부여
+                TODO: key generate 모델 학습을 더 어렵게 만들기 (O)
+    TODO : 데이터셋 위치 바꾸기
 """
 
 def main(user_input, stage='train'):
@@ -27,16 +27,16 @@ def main(user_input, stage='train'):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # load audio data
-    dataset = augment_sound_data(['sample1.wav', 'sample2.wav', 'sample3.wav', 'sample4.wav'])
+    dataset = augment_sound_data('dataset/train')
     dataset = SoundDataset(dataset)
     
     model = CryptoModel().to(device)
     if stage == 'train':
         model.trainer(getattr(config, 'training'), dataset, user_input)
     elif stage == 'test':    
-        model.test(getattr(config, 'inference'), 'compare2.wav', user_input)
+        model.test(getattr(config, 'inference'), 'dataset/test/compare2.wav', user_input)
     elif stage == 'inference':
-        model.inference(getattr(config, 'inference'), 'sample1.wav', user_input)    
+        model.inference(getattr(config, 'inference'), 'dataset/test/sample1.wav', user_input)    
     return
 
 if __name__ == "__main__":
